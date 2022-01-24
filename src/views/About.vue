f8ab727a9d5dbae0cb3985d84e58dfa7e525b084
Adrien <adrien.mollat@orange.fr>
2022-01-24 16:54:55 +0100

    Routing commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, quae suscipit voluptates eius ut dicta, tenetur ad vero ipsum quas excepturi veniam incidunt exercitationem. Animi odit, adipisci ducimus totam molestiae sed. Neque maxime, laborum quia totam distinctio amet adipisci autem. Vero totam ea delectus neque ratione ut, harum praesentium, officia fugit quibusdam autem dolor numquam quos at facere ipsum aliquid odio provident quia expedita molestiae ullam nam commodi. Nostrum ratione maiores repellat nulla expedita similique doloremque fugiat laudantium, itaque distinctio suscipit unde voluptatibus eligendi! Aliquam harum esse, magni cumque deserunt, sit quam nostrum nemo, eos totam est perferendis aliquid sint.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, quae suscipit voluptates eius ut dicta, tenetur ad vero ipsum quas excepturi veniam incidunt exercitationem. Animi odit, adipisci ducimus totam molestiae sed. Neque maxime, laborum quia totam distinctio amet adipisci autem. Vero totam ea delectus neque ratione ut, harum praesentium, officia fugit quibusdam autem dolor numquam quos at facere ipsum aliquid odio provident quia expedita molestiae ullam nam commodi. Nostrum ratione maiores repellat nulla expedita similique doloremque fugiat laudantium, itaque distinctio suscipit unde voluptatibus eligendi! Aliquam harum esse, magni cumque deserunt, sit quam nostrum nemo, eos totam est perferendis aliquid sint.</p>
+
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1> Homepage </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam ipsam, beatae neque tempora voluptatibus totam, in, vitae animi doloremque rem veritatis cupiditate dolores autem saepe dolorem facere voluptate sint inventore quidem. Dolorum dolorem, error eos quod quia dignissimos quasi sunt culpa, harum esse vitae, fugiat laboriosam minima id labore voluptas voluptatum omnis placeat odio ducimus ex impedit doloribus explicabo. Quas a non, repudiandae itaque in asperiores vel culpa modi omnis rem voluptatum error nisi, veritatis consequatur! Nostrum, adipisci eos. Perferendis corporis facilis ab? Quis repudiandae architecto maiores praesentium, vero inventore, magnam accusamus error aspernatur nisi nemo. Iste explicabo molestias cupiditate.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam ipsam, beatae neque tempora voluptatibus totam, in, vitae animi doloremque rem veritatis cupiditate dolores autem saepe dolorem facere voluptate sint inventore quidem. Dolorum dolorem, error eos quod quia dignissimos quasi sunt culpa, harum esse vitae, fugiat laboriosam minima id labore voluptas voluptatum omnis placeat odio ducimus ex impedit doloribus explicabo. Quas a non, repudiandae itaque in asperiores vel culpa modi omnis rem voluptatum error nisi, veritatis consequatur! Nostrum, adipisci eos. Perferendis corporis facilis ab? Quis repudiandae architecto maiores praesentium, vero inventore, magnam accusamus error aspernatur nisi nemo. Iste explicabo molestias cupiditate.</p>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
